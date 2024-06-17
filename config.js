bdbaaab900e47e7ea1d724d5be4211d290916025
Ozysy <artgagarin2@gmail.com>
2024-06-17 16:09:04 +0400

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+export default{
+    JWT_SECRET : "gDEtL3vLc1swKJEFZIl1xof9QSTFB09B7BxUXmHRx3t70y2SEKaPHAj6umOlWeb5",
+    EMAIL: "della87@ethereal.email",
+    PASSWORD: "bjs8f2keCnSU6Z8gc8",
+    ATLAS_URI: "mongodb+srv://artgagarin2:admine1234@cluster0.lp1xz4u.mongodb.net/"
+}
+// admine1234
